ca9b42091720f34c8c92b61aceb6ea27e6bd2970
Juan <juancarloscamacho2809@gmail.com>
2024-06-06 11:42:51 -0400

    Methods y computed

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  
     <!--Componentización v-model, v-if, two binding un conjunto modelo de datos todos-->
     <dummy-component>
       hola te quedan {{ todos.length }} cosas por hacer..
@@ -21,14 +22,11 @@
     :form-content="newTodoForm" 
     :submit-handler="addTodo" />
 
-   <button-base  text="Enviar" 
-   :context="primery" :size="lg" 
-   :onClick="sayHello" class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"/>
-   <button-base  text="Enviar" 
-   :context="secundary" 
-   :size="lg" 
-   :onClick="sayGoobye" />
-   <button-base  text="'Enviar'" />
+   <button-base  text="counter" :context="primery" :size="lg" :onClick="sayHello" class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"/>
+   <button-base  text="conuterBy2" :context="primery" :size="lg" :onClick="sayHello" class="text-white bg-gray-700 hover:bg-gray-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-600 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"/>
+  
+   <button-base  text="Enviar" :context="secundary" :size="lg" :onClick="sayGoobye" />
+   <button-base  text="menssageToUpper" />
    
  
 </template>
@@ -62,6 +60,7 @@ export default {
 
   data() {
     return {
+      counter: 0,
       todos: [
         {
           title: "prueba",
@@ -111,8 +110,15 @@ export default {
         console.log("adios...")
   }
 },
+computed:{
+  conuterBy2(){
+    return this.counter * 2
+  },
+  menssageToUpper(){
+    return this.menssage.ToUpperCase()
+  }
 }
-  
+}
   
  
 
